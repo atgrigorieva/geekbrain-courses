11fd2c9c6c7984f7bc32183f1fdae76bc84998a0
atgrigorieva <flora992@yandex.ru>
2018-06-08 15:36:46 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,6 +1,4 @@
 var mArrRus = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 var mArrEng = ["January", "February", "марта", "March", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-var remaining = new Date("December 31 2018") -  new Date(process.argv[2] + " " + mArrEng[mArrRus.indexOf(process.argv[3])] + " " + process.argv[4]); // миллисекунды до даты
-
-console.log(remaining/(1000 * 60 * 60 * 24));
+console.log((new Date("December 31 " + process.argv[4]) -  new Date(process.argv[2] + " " + mArrEng[mArrRus.indexOf(process.argv[3])] + " " + process.argv[4]))/(1000 * 60 * 60 * 24));
